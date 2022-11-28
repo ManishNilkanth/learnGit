f269d28608e3403ab5d24ddaec3a96dafa27c128
Manish Nilkanth <nilkanthmanish0@gmail.com>
2022-11-28 22:32:03 +0530

    added multip.java

---
+++ multip.java
@@ -0,0 +1,12 @@
+public class multip {
+    
+    public static void main(String[] args)
+    {
+        int a = 12;
+        int b = 32;
+        int sum = a + b;
+        int multi = a * b;
+        System.out.println("the milti :"+ multi);
+        System.out.println("the sum is :"+sum);
+    }
+}
