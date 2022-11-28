c01ad368a28100cd8919fce512b19572e6a9f568
Manish Nilkanth <nilkanthmanish0@gmail.com>
2022-11-28 21:46:26 +0530

    initial commit

---
+++ program.java
@@ -0,0 +1,10 @@
+public class program
+{
+    public static void main(String[] args)
+    {
+        int a = 12;
+        int b = 32;
+        int sum = a + b;
+        System.out.println("the sum is :"+sum);
+    }
+}
